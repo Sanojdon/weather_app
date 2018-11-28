7112d994d4adb7af65d9fdb5b23810d43f36e544
sanojdon <sanoj.mv.93@gmail.com>
2018-11-28 10:34:36 +0530

    More error messages for the end user.

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import json
 from models import db, Record
 from datetime import datetime, timedelta
 import sys
+import logging
+
+logging.basicConfig(filename='weather_update.log', level=logging.DEBUG)
 
 app = Flask(__name__)
 
@@ -16,6 +19,8 @@ def result(addr, dfro=None, dend=None):
 	if dfro is None and dend is None:
 		dfro = datetime.now().strftime("%Y-%m-%d")
 		dend = (datetime.now()+ timedelta(days=1)).strftime("%Y-%m-%d")
+
+	logging.warning('If the dates is more than 1 day apart, you will get error')
 	query = "https://api.weatherbit.io/v2.0/history/daily?city="+addr+"&start_date="+dfro+"&end_date="+dend+"&key="+key
 
 	try:
@@ -25,7 +30,7 @@ def result(addr, dfro=None, dend=None):
 		fin['city'] = data['city_name']
 		return fin
 	except:
-		return "Connection Failure.."
+		return 0
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -34,25 +39,31 @@ def index():
 		place = request.args.get('place')
 		dfro = request.args.get('date_start')
 		dend = request.args.get('date_end')
+		error=0
+		logging.info('The inputs from the user has been recieved on the server side')
 		if place is not None:
 			r = result(place)
 			if(dfro is not '' and dend is not ''):
 				r = result(place,dfro,dend)
 
-			context = dict()
-			context['city'] = r['city']
-			context['temp'] = r['temp']
-			context['max_temp'] = r['max_temp']
-			context['min_temp'] = r['min_temp']
-			context['pressure'] = r['pres']
-			context['datetime'] = r['datetime']
+			if r != 0:
+				context = dict()
+				context['city'] = r['city']
+				context['temp'] = r['temp']
+				context['max_temp'] = r['max_temp']
+				context['min_temp'] = r['min_temp']
+				context['pressure'] = r['pres']
+				context['datetime'] = r['datetime']
 
-			if request.args.get('btnSave') is not None:
-				record = Record(context['city'], context['temp'], context['max_temp'], context['min_temp'], context['pressure'], context['datetime'])
-				db.session.add(record)
-				db.session.commit()
-			return render_template('/result.html', context=context)
-	return render_template('/index.html')
+				if request.args.get('btnSave') is not None:
+					record = Record(context['city'], context['temp'], context['max_temp'], context['min_temp'], context['pressure'], context['datetime'])
+					db.session.add(record)
+					db.session.commit()
+				return render_template('/result.html', context=context)
+			else:
+				error = "Connectivity Issue.. Please check your internet connectivity!!!"
+
+	return render_template('/index.html', error=error)
 
 @app.route('/saved')
 def saved():
